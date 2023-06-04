e44ed1fe5abeb0478d19c91a52ee5845555f97bb
DupieM <221326@virtualwindow.co.za>
2023-06-04 23:13:16 +0200

    Visual style part 1

---
+++ appointment.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Appointment</title>
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div id="container2">
+            <div class=topnav>
+            <a href=""><img src="" alt="logo"></a>
+            <a href="docters.php"><img src="img\icon_doc.png" alt="doc">Doctors</a>
+            <a href="patients.php"><img src="img\icon_pat.png" alt="pat">Patients</a>
+            </div>    
+        </div>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Log In</title>
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div id="container">
+            <img src="img\dv logo.png" alt="Logo" class="logo">
+    
+            <img src="img\login.png" alt="image of patern" class="patern">
+        </div>
+        
+    </body>
+</html>
